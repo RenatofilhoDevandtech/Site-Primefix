01ca6231214b199aff893e8c7c14956c563498d3
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2025-09-03 00:49:58 -0300

    refactor: � atualiza ponto de entrada da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { MovieProvider } from './contexts/MovieContext';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <MovieProvider>
+        <App />
+      </MovieProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
