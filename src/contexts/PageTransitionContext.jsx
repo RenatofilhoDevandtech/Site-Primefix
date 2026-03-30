6343903332f3f7ec83547f6693b4f01b48f4e284
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2026-03-30 20:10:04 -0300

    feat: :running: atualiza contexto de transições

--- src/contexts/PageTransitionContext.jsx
+++ src/contexts/PageTransitionContext.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
-/* eslint-disable react/prop-types */
 import { createContext, useContext, useState } from 'react';
 
 // 1. Cria o Contexto
