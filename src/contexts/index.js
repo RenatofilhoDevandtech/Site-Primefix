f2d43d8ac3c27b24d79a87468539c2eb8f886ca5
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2026-03-30 20:10:22 -0300

    chore: :file_folder: adiciona export centralizado de contextos

---
+++ src/contexts/index.js
@@ -0,0 +1,6 @@
+// Central export for all contexts
+export { AuthContext } from './auth-context';
+export { AuthProvider } from './AuthProvider';
+export { MovieProvider, useMovies } from './MovieContext';
+export { NotificationProvider, NotificationContext } from './NotificationContext';
+export { default as PageTransitionContext, PageTransitionProvider, usePageTransition } from './PageTransitionContext';
